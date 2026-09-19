e5574ba7d5633b6d09d91c56bf6f931aadac7422
Marcos Ashton Iglesias <marcosashiglesias@gmail.com>
2026-09-19 19:05:29 +0100

    use the library fifo

--- src/host_fifo.ml
+++ src/host_fifo.ml
@@ -25,27 +25,19 @@ module O = struct
 end
 
 let create (scope : Scope.t) (i : Signal.t I.t) =
-  let spec = Clocking.to_spec i.clocking in
-  let%hw_var level = Always.Variable.reg spec ~width:level_bits in
-  let%hw_var rd = Always.Variable.reg spec ~width:(Int.ceil_log2 depth) in
-  let%hw_var wr = Always.Variable.reg spec ~width:(Int.ceil_log2 depth) in
-  let%hw empty = level.value ==:. 0 in
-  let%hw full = level.value ==:. depth in
-  let%hw push = i.push.valid &: ~:full in
-  let%hw pop = i.pop &: ~:empty in
-  let slots =
-    List.init depth ~f:(fun slot ->
-      reg spec ~enable:(push &: (wr.value ==:. slot)) i.push.value)
+  let fifo =
+    Fifo.create
+      ~scope
+      ~showahead:true
+      ()
+      ~capacity:depth
+      ~clock:i.clocking.clock
+      ~clear:i.clocking.clear
+      ~wr:i.push.valid
+      ~d:i.push.value
+      ~rd:i.pop
   in
-  let%hw head = mux rd.value slots in
-  Always.(
-    compile
-      [ when_ push [ wr <-- wr.value +:. 1 ]
-      ; when_ pop [ rd <-- rd.value +:. 1 ]
-      ; when_ (push &: ~:pop) [ level <-- level.value +:. 1 ]
-      ; when_ (pop &: ~:push) [ level <-- level.value -:. 1 ]
-      ]);
-  { O.head; level = level.value; empty; full }
+  { O.head = fifo.q; level = fifo.used; empty = fifo.empty; full = fifo.full }
 ;;
 
 let hierarchical ?instance scope i =
