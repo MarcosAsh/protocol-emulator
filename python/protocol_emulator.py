acedf355a03d0b93e8bd9a1c8d7cc01a9b0297b1
Marcos Ashton Iglesias <marcosashiglesias@gmail.com>
2026-09-20 15:40:43 +0100

    add in_count to the host library

--- python/protocol_emulator.py
+++ python/protocol_emulator.py
@@ -15,7 +15,7 @@ PROGRAM = 0x0A
 CONFIG = 0x10
 
 CONFIG_FIELDS = [
-    "side_set_count", "side_set_base", "side_set_pindirs", "in_base", "out_base",
+    "side_set_count", "side_set_base", "side_set_pindirs", "in_base", "in_count", "out_base",
     "out_count", "set_base", "set_count", "jmp_pin", "capture_pin", "capture_rising",
     "in_shift_right", "out_shift_right", "autopush", "push_threshold", "autopull",
     "pull_threshold", "crc_width", "crc_poly", "crc_init", "crc_reflect",
@@ -23,7 +23,7 @@ CONFIG_FIELDS = [
 ]
 
 DEFAULT_CONFIG = {
-    "side_set_base": 5, "out_base": 5, "out_count": 1, "set_base": 5, "set_count": 1,
+    "side_set_base": 5, "in_count": 16, "out_base": 5, "out_count": 1, "set_base": 5, "set_count": 1,
     "in_shift_right": 1, "out_shift_right": 1, "push_threshold": 16, "pull_threshold": 16,
     "crc_width": 16, "crc_poly": 0xA001, "crc_init": 0xFFFF, "crc_reflect": 1,
     "stuff_level": 1,
