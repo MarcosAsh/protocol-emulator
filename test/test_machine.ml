aa25e22c084a38671912b7beb4f36d3089549795
Marcos Ashton Iglesias <marcosashiglesias@gmail.com>
2026-09-20 15:45:45 +0100

    receive two usb packets in the model

--- test/test_machine.ml
+++ test/test_machine.ml
@@ -569,3 +569,60 @@ let%expect_test "usb tx builds the crc and stuffs the get descriptor packet" =
       ((underflow false) (overflow false) (missed_deadline false) (decode false))))
     |}]
 ;;
+
+let%expect_test "usb rx decodes, unstuffs and checks two packets" =
+  let bit_period = 32 in
+  let data = [ 0x80; 0x06; 0x00; 0x01; 0x00; 0x00; 0x40; 0x00 ] in
+  let crc = Usb_ls.crc16 (Usb_ls.bits_of_bytes data) in
+  let data0 = (0xc3 :: data) @ [ crc land 0xff; crc lsr 8 ] in
+  let ones = [ 0xc3; 0xff; 0x00; 0xff ] in
+  let line_levels packet =
+    List.concat_map
+      (Usb_ls.encode packet @ List.init 4 ~f:(fun _ -> Usb_ls.Line.J))
+      ~f:(fun line ->
+        let dp, dm =
+          match line with
+          | J -> 0, 1
+          | K -> 1, 0
+          | Se0 -> 0, 0
+        in
+        List.init bit_period ~f:(fun _ ->
+          (dp lsl usb_rx_dp_pin) lor (dm lsl usb_rx_dm_pin)))
+  in
+  let levels =
+    List.init 40 ~f:(fun _ -> 1 lsl usb_rx_dm_pin) @ line_levels data0 @ line_levels ones
+  in
+  let t =
+    Machine.create
+      ~config:usb_rx_config
+      ~program:(assemble (usb_rx ~half_period:(bit_period / 2)))
+    |> ok_exn
+  in
+  let t = Machine.write_tx t bit_period |> ok_exn in
+  let t, words =
+    List.fold levels ~init:(t, []) ~f:(fun (t, words) inputs ->
+      let t = Machine.step t ~inputs in
+      match Machine.read_rx t with
+      | Some (w, t) -> t, w :: words
+      | None -> t, words)
+  in
+  let words = List.rev words in
+  let bytes = List.map words ~f:(fun w -> w lsr 8) in
+  let residuals =
+    List.filteri words ~f:(fun i _ ->
+      i = List.length data0 + 1 || i = List.length words - 1)
+  in
+  let expected = List.map [ data0; ones ] ~f:Usb_ls.residual in
+  print_s
+    [%message
+      (bytes : int list)
+        (residuals : Int.Hex.t list)
+        (expected : Int.Hex.t list)
+        (t.fault : Machine.Fault.t)];
+  [%expect {|
+    ((bytes (128 195 128 6 0 1 0 0 64 0 221 148 140 128 195 255 0 255 234))
+     (residuals (0x8ce4 0xea69)) (expected (0x8ce4 0xea69))
+     (t.fault
+      ((underflow false) (overflow false) (missed_deadline false) (decode false))))
+    |}]
+;;
