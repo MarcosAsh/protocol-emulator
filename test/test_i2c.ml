6115e8c83ad3532e0cde4ea25713d4066ee8832b
Marcos Ashton Iglesias <marcosashiglesias@gmail.com>
2026-09-21 14:37:51 +0100

    move the i2c lockstep tests next to the model tests

--- test/test_i2c.ml
+++ test/test_i2c.ml
@@ -211,3 +211,127 @@ let%expect_test "one core polls the slave over i2c and logs over uart" =
       ((underflow false) (overflow false) (missed_deadline false) (decode false))))
     |}]
 ;;
+
+let%expect_test "i2c master in lockstep" =
+  let memory = Array.create ~len:16 0 in
+  let slave = ref (I2c_slave.create ~address:0x50 ~memory) in
+  let words =
+    [ i2c_word ~start:true 0xa0; i2c_word 3; i2c_word ~stop:true 0xaa ] |> ref
+  in
+  let bus (m : Machine.t) =
+    let master_sda = 1 - ((m.pin_dir lsr sda) land 1) in
+    let bus_sda = if I2c_slave.drive_low !slave then 0 else master_sda in
+    let bus_scl = 1 - ((m.pin_dir lsr scl) land 1) in
+    bus_sda, bus_scl
+  in
+  let model = ref None in
+  let (_ : Machine.t) =
+    Lockstep.lockstep
+      ~cycles:1500
+      ~config:i2c_config
+      ~program:(assemble (i2c_master ~quarter:8))
+      ~inputs:(fun _ ->
+        match !model with
+        | None -> (1 lsl sda) lor (1 lsl scl)
+        | Some m ->
+          let bus_sda, bus_scl = bus m in
+          (bus_sda lsl sda) lor (bus_scl lsl scl))
+      ~host:(fun _ ->
+        match !words with
+        | w :: rest ->
+          words := rest;
+          { Lockstep.Host.idle with tx = Some w; pop_rx = true }
+        | [] -> { Lockstep.Host.idle with pop_rx = true })
+      ~react:(fun m ->
+        let bus_sda, bus_scl = bus m in
+        slave := I2c_slave.step !slave ~sda:bus_sda ~scl:bus_scl;
+        model := Some m)
+      ()
+  in
+  let log = I2c_slave.log !slave in
+  print_s [%message (log : string list) (memory : int array)];
+  [%expect
+    {|
+    ("lockstep held" (cycles 1500))
+    ((log (start "address 80 write" "pointer 3" "write 170" stop))
+     (memory (0 0 0 170 0 0 0 0 0 0 0 0 0 0 0 0)))
+    |}]
+;;
+
+let%expect_test "i2c slave in lockstep" =
+  let master =
+    ref
+      (I2c_peer.create
+         ~quarter:8
+         [ Start
+         ; Write 0xa0
+         ; Write 3
+         ; Start
+         ; Write 0xa1
+         ; Read { ack = true }
+         ; Read { ack = false }
+         ; Stop
+         ])
+  in
+  let bus_sda (m : Machine.t) =
+    I2c_peer.sda !master land (1 - ((m.pin_dir lsr sda) land 1))
+  in
+  let last_sda = ref 1 in
+  let model =
+    Lockstep.lockstep
+      ~cycles:3000
+      ~config:i2c_slave_config
+      ~program:(assemble i2c_slave)
+      ~preload:[ 0x50 lsl 1; 0x12; 0x34 ]
+      ~inputs:(fun _ -> (!last_sda lsl sda) lor (I2c_peer.scl !master lsl scl))
+      ~react:(fun m ->
+        last_sda := bus_sda m;
+        master := I2c_peer.step !master ~sda:!last_sda)
+      ()
+  in
+  print_s
+    [%message
+      (I2c_peer.log !master : string list)
+        (List.length model.rx_fifo : int)
+        (model.pc : int)];
+  [%expect
+    {|
+    ("lockstep held" (cycles 3000))
+    (("I2c_peer.log (!master)" (ack ack ack "read 18" "read 52"))
+     ("List.length model.rx_fifo" 3) (model.pc 4))
+    |}]
+;;
+
+let%expect_test "i2c logger in lockstep" =
+  let memory = Array.init 16 ~f:(fun i -> 0x10 + (0x11 * i)) in
+  let slave = ref (I2c_slave.create ~address:0x50 ~memory) in
+  let levels = ref [] in
+  let bus_sda (m : Machine.t) =
+    if I2c_slave.drive_low !slave then 0 else 1 - ((m.pin_dir lsr sda) land 1)
+  in
+  let last_sda = ref 1 in
+  let last_scl = ref 1 in
+  let (_ : Machine.t) =
+    Lockstep.lockstep
+      ~cycles:3000
+      ~config:i2c_logger_config
+      ~program:(assemble i2c_logger)
+      ~inputs:(fun _ -> (!last_sda lsl sda) lor (!last_scl lsl scl))
+      ~react:(fun m ->
+        last_sda := bus_sda m;
+        last_scl := 1 - ((m.pin_dir lsr scl) land 1);
+        slave := I2c_slave.step !slave ~sda:!last_sda ~scl:!last_scl;
+        levels := ((m.pin_out lsr logger_uart_pin) land 1) :: !levels)
+      ()
+  in
+  let logged = decode_uart (List.rev !levels) ~period:16 in
+  print_s [%message (logged : int list) (I2c_slave.log !slave : string list)];
+  [%expect
+    {|
+    ("lockstep held" (cycles 3000))
+    ((logged (16 33 50))
+     ("I2c_slave.log (!slave)"
+      (start "address 80 read" nack stop start "address 80 read" nack stop start
+       "address 80 read" nack stop start "address 80 read" nack)))
+    |}]
+;;
